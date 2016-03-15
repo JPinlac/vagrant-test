13dbfcc7a38ad4134984e6b49b144120d65a37aa
Jonathan Pinlac <jonppinlac@gmail.com>
2016-03-15 15:22:21 -0400

    Default rails app setup

--- ruby-ionic-env.sh
+++ ruby-ionic-env.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 sudo apt-get update
 sudo apt-get install git -y
@@ -10,4 +10,3 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo npm install -g ionic
 sudo npm install -g cordova
-sudo apt-get install openjdk-7-jdk -y
