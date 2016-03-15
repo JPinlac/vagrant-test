564d8aa66b8a58b9260944547aaff611d6085f47
Jonathan Pinlac <jonppinlac@gmail.com>
2016-03-15 17:27:30 +0000

    Provision node, ionic, cordova

--- ruby-ionic-env.sh
+++ ruby-ionic-env.sh
@@ -1,7 +1,13 @@
 #!usr/bin/env bash
 
 sudo apt-get update
+sudo apt-get install git -y
 sudo apt-get install sqlite3 -y
 command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 source /home/vagrant/.rvm/scripts/rvm
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo npm install -g ionic
+sudo npm install -g cordova
+sudo apt-get install openjdk-7-jdk -y
